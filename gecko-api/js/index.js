23a3b136770e637008afd8e83516d214ead4dfa2
RydCri <107228115+RydCri@users.noreply.github.com>
2023-04-10 17:16:03 -0700

    chartRender cant find element

--- gecko-api/js/index.js
+++ gecko-api/js/index.js
@@ -56,6 +56,67 @@ const getTrending = async () => {
             console.error(e);
         }
     }
+
+
+// var options = {
+//     series: [{
+//         name: 'XYZ MOTORS',
+//         data: dates
+//     }],
+//     chart: {
+//         type: 'area',
+//         stacked: false,
+//         height: 350,
+//         zoom: {
+//             type: 'x',
+//             enabled: true,
+//             autoScaleYaxis: true
+//         },
+//         toolbar: {
+//             autoSelected: 'zoom'
+//         }
+//     },
+//     dataLabels: {
+//         enabled: false
+//     },
+//     markers: {
+//         size: 0,
+//     },
+//     title: {
+//         text: 'Stock Price Movement',
+//         align: 'left'
+//     },
+//     fill: {
+//         type: 'gradient',
+//         gradient: {
+//             shadeIntensity: 1,
+//             inverseColors: false,
+//             opacityFrom: 0.5,
+//             opacityTo: 0,
+//             stops: [0, 90, 100]
+//         },
+//     },
+//     yaxis: {
+//         labels: {
+//             formatter: coinSparkline,
+//             },
+//         },
+//         title: {
+//             text: 'Price'
+//         },
+//     },
+//     xaxis: {
+//         type: 'datetime',
+//     },
+//     tooltip: {
+//         shared: false,
+//         y: {
+//             formatter: coinSparkline,
+//             }
+//         }
+//     }
+// };
+
     const getChart = async (url) => {
     try {
     $.getJSON(url)
@@ -99,6 +160,72 @@ const getTrending = async () => {
                 }).format(coin.low_24h)
                 let color = coin.price_change_percentage_24h > 0 ? 'green' : 'red';
 
+                function chartRender(input,prices) {
+                    let sparkOptions = {
+                        series: [{
+                            name: 'XYZ MOTORS',
+                            data: prices
+                        }],
+                        chart: {
+                            type: 'area',
+                            stacked: false,
+                            height: 350,
+                            zoom: {
+                                type: 'x',
+                                enabled: true,
+                                autoScaleYaxis: true
+                            },
+                            toolbar: {
+                                autoSelected: 'zoom'
+                            }
+                        },
+                        dataLabels: {
+                            enabled: false
+                        },
+                        markers: {
+                            size: 0,
+                        },
+                        title: {
+                            text: 'Stock Price Movement',
+                            align: 'left'
+                        },
+                        fill: {
+                            type: 'gradient',
+                            gradient: {
+                                shadeIntensity: 1,
+                                inverseColors: false,
+                                opacityFrom: 0.5,
+                                opacityTo: 0,
+                                stops: [0, 90, 100]
+                            },
+                        },
+                        yaxis: {
+                            labels: {
+                                formatter: function (val) {
+                                    return (val / 1000000).toFixed(0);
+                                },
+                            },
+                            title: {
+                                text: 'Price'
+                            },
+                        },
+                        xaxis: {
+                            type: 'datetime',
+                        },
+                        tooltip: {
+                            shared: false,
+                            y: {
+                                formatter: function (val) {
+                                    return (val / 1000000).toFixed(0)
+                                }
+                            }
+                        }
+                    };
+                    return new ApexCharts(document.getElementById(input), sparkOptions).render()
+
+                }
+
+                    // let chart = new ApexCharts(prices, sparkOptions);
                 let chartElement = "";
                 chartElement +=
 `<tr>
@@ -111,10 +238,9 @@ const getTrending = async () => {
 <td class="coin-marketcap">${marketCap}</td>
 <td class="coin-high">${high}</td>
 <td class="coin-low">${low}</td>
- </tr>`
-                // api: MC rank, Name, Price, 1h/24h/7d price change, 24h volume, mkt cap,
-                //     market_cap_rank, id, current_price, price_change_percentage_24h, total_volume, market_cap
+<canvas id="${coin.name}-sparkline">${chartRender(`${coin.name}-sparkline`, coin.sparkline_in_7d.price)}</canvas>`
                 $('#coinChart').append(chartElement)
+
             })
         })
         }
@@ -122,7 +248,53 @@ const getTrending = async () => {
     console.error(e)
     }
 }
-
+// async function renderChart(canvas) {
+//
+//     const dates = coin.prices.map((price) => new Date(price[0]).toLocaleDateString());
+//     const prices = coin.prices.map((price) => price[1]);
+//
+//     const chart = new Chart(canvas, {
+//         type: "line",
+//         data: {
+//             labels: dates,
+//             datasets: [
+//                 {
+//                     data: prices,
+//                     borderColor: "rgb(75, 192, 192)",
+//                     backgroundColor: "rgba(75, 192, 192, 0.2)", // Add this line to show a background color
+//                     borderWidth: 1, // Adjust line width
+//                     tension: 0.1,
+//                 },
+//             ],
+//         },
+//         options: {
+//             plugins: {
+//                 legend: {
+//                     display: false,
+//                 },
+//             },
+//             scales: {
+//                 x: {
+//                     display: false,
+//                     ticks: {
+//                         autoSkip: true,
+//                         maxTicksLimit: 4,
+//                     },
+//                 },
+//                 y: {
+//                     display: false,
+//                     position: "right",
+//                 },
+//             },
+//             elements: {
+//                 point: {
+//                     radius: 0, // Hide data points
+//                 },
+//             },
+//             maintainAspectRatio: false, // Allow the chart to resize with the container
+//         },
+//     });
+// }
 const searchQuery = async (input) => {
     $('#searchResults').empty()
     try {
@@ -198,9 +370,9 @@ getGas()
 $('#search').change(function (){
 $('#searchResults').empty()
 })
-getChart("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
-    getTicker("https://api.coingecko.com/api/v3/simple/price?ids=shiba-inu&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&precision=full")
-    getTicker("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&precision=full")
-    getTicker("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&precision=full")
-
+// getChart("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+    // getTicker("https://api.coingecko.com/api/v3/simple/price?ids=shiba-inu&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&precision=full")
+    // getTicker("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&precision=full")
+    // getTicker("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&precision=full")
+getChart('../mockdb/sparkline.json')
 // getTrending()
