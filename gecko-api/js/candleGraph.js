0703ad9662495d5812ceb25078bf16ce45fbb610
RydCri <107228115+RydCri@users.noreply.github.com>
2023-04-15 16:32:40 -0700

    getOHLC is now agnostic, the candleGraph.js doesn't need to be sourced to any page it's now just a placeholder asset to be incorporated wherever you want it.
    As of now, it's being used in the Show page

--- gecko-api/js/candleGraph.js
+++ gecko-api/js/candleGraph.js
@@ -1,58 +1,84 @@
 
-$.ajax('https://api.coingecko.com/api/v3/coins/bitcoin/ohlc?vs_currency=usd&days=1').done(function (candles) {
 
-    let options2 = {
-        series: [{
-            data: Array.from({length: candles.length}, () => ({
-                x: null,
-                y: [[], [], [], []]
-            }))
-        }]
-        ,
-        chart: {
-            type: 'candlestick',
-            height: 350
-        },
-        title: {
-            text: 'CandleStick Chart',
-            align: 'left'
-        },
-        xaxis: {
-            type: 'datetime'
-        },
-        yaxis: {
-            tooltip: {
-                enabled: true
+let chart;
+const getOHLC = async (coin, days) => {
+    try {
+        const candle = await $.getJSON(`https://api.coingecko.com/api/v3/coins/${coin}/ohlc?vs_currency=usd&days=${days}`);
+        const coinData = await $.getJSON(`https://api.coingecko.com/api/v3/coins/${coin}?localization=false&tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true`)
+
+        const dataPoints = candle.map((candles) => {
+            return {
+                x: new Date(candles[0]),
+                y: [candles[1], candles[2], candles[3], candles[4]]
+            };
+        });
+
+            let chartType = () => {
+                if (days == 1){
+                    return `${coinData.name} 24 hour`
+                }else if(days > 1){
+                    return `${coinData.name}` + days + ` day`
+                }
+                    }
+        const options2 = {
+            series: [{
+                data: dataPoints
+            }],
+            chart: {
+                type: 'candlestick',
+                height: 350
+            },
+            theme: {
+                monochrome: {
+                    enabled: true,
+                    color: '#255aee',
+                    shadeTo: 'light',
+                    shadeIntensity: 0.65
+                }
+            },
+            title: {
+                text: chartType(),
+                align: 'left'
+            },
+            xaxis: {
+                type: 'datetime'
+            },
+            yaxis: {
+                tooltip: {
+                    enabled: true
+                }
             }
+        };
+
+        if (chart) {
+            chart.destroy();
         }
+
+        chart = new ApexCharts($("#liveChart")[0], options2);
+        chart.render();
+    } catch (e) {
+        console.error(e);
     }
-
-    for (let i = 0; i < candles.length; i++) {
-
-        options2.series[0].data[i].x = new Date(candles[i][0] * 1000);
-        options2.series[0].data[i].y[0].push(candles[i][1]);
-        options2.series[0].data[i].y[1].push(candles[i][2]);
-        options2.series[0].data[i].y[2].push(candles[i][3]);
-        options2.series[0].data[i].y[3].push(candles[i][4]);
-    }
-    console.log(options2.series[0].data[1])
-
-
-    let chart = new ApexCharts(document.querySelector("#chart"), options2);
-    chart.render();
-})
+};
 
 
 
+//event listeners for buttons, hardcoded atm
+$("#btn1").click(() => {
+    getOHLC('bitcoin', '1');
+});
 
+$("#btn14").click(() => {
+    getOHLC('bitcoin', '14');
+});
+$("#btn30").click(() => {
+    getOHLC('bitcoin', '30');
+});
 
+$("#btn90").click(() => {
+    getOHLC('bitcoin', '90');
+});
 
-//todo refernce
-// console.log(options2.series[0].data[0].x) // this is the date
-// console.log(options2.series[0].data[0].y) //this is 0-3 based on the number
-// console.log(options2.series[0].data[1].x) // this is the date
-// console.log(options2.series[0].data[1].y) //this is 0-3 based on the number
-// console.log(options2.series[0].data[1].y[1])
 
 
 
